2f92502c4745b0c0107624da68540bc3168bb8da
scotty <scottyndanga02400@gmail.com>
2025-10-27 09:58:38 +0100

    add comment

--- EXERCICE/boucles_sauts_conditionnels/Exercice_3.s
+++ EXERCICE/boucles_sauts_conditionnels/Exercice_3.s
@@ -2,14 +2,14 @@
     .global _start
 
 _start:
-    mov R0, #8
+    mov R0, #8      @ compteur = 8
 
 loop:
-    cmp R0, #0
+    cmp R0, #0      @ comparer R0 à 0
     Beq stop        @ si R0 == 0, on sort
-    sub R0, R0, #1
+    sub R0, R0, #1  @ R0 = R0 - 1
     b loop
 
 stop:
-    mov R7, #1
+    mov R7, #1    @ syscall exit
     svc #0
